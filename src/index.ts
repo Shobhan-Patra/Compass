43de051875797a8ab88a17a3473c57f382422485
Shobhan Patra <230170146055@vgecg.ac.in>
2025-12-09 15:52:31 +0530

    Test prettier

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { eq } from 'drizzle-orm';
 import { usersTable } from './db/schema.ts';
-  
+
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
   const user: typeof usersTable.$inferInsert = {
-    name :  "John",
+    name: 'John',
     age: 30,
     email: 'john@example.com',
-  	};
+  };
 
-  	await db.insert(usersTable).values(user);
-  		console.log('New user created!')
+  await db.insert(usersTable).values(user);
+  console.log('New user created!');
 
   const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ",     users)
+  console.log('Getting all users from the database: ', users);
   /*
   const users: {
     id: number;
@@ -32,10 +32,10 @@ async function main() {
       age: 31,
     })
     .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
+  console.log('User info updated!');
 
   await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
+  console.log('User deleted!');
 }
 
 main();
